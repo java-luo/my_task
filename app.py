eff60500d839972ec94d4dc562c409f466c99fa0
lansen <m18031069724@163.com>
2020-06-04 17:31:46 +0800

    '国博票量查询页面'

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,request
 from lansen.service import mainController
 import json
+from lansen.util import SpiderUtil
 from  lansen.renwu import Const
+import  requests
 app = Flask(__name__)
 
 @app.route('/createTask',methods=['POST'])
@@ -26,12 +28,24 @@ def removeTask():
     isOk=mainController.removeTask(taskName)
     return str(isOk)
 
+@app.route('/getTicket')
+def getTicket():
+    headers = {
+        "Referer": "http://ticket.chnmuseum.cn/yuyue/index",
+        "Origin": "http://ticket.chnmuseum.cn",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+        "User-Agent": SpiderUtil.get_user_agent()
+    }
+    r=requests.get("https://ticketapi.chnmuseum.cn/api/ticket/calendar?p=w",headers=headers)
+    return r.json()
+
 def getTask(taskName):
     task = Const.taskList.get(taskName)
     if(task==None):
         return True
     else:
         return False
+
 
 
 if __name__ == '__main__':
